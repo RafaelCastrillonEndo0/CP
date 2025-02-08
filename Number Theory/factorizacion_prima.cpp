d79612519168cf3eaac771d580c319d69ce03263
RafaelCastrillonEndo0 <castrillonrafael7@gmail.com>
2025-02-07 23:27:02 -0500

    Update factorizacion_prima.cpp

--- Number Theory/factorizacion_prima.cpp
+++ Number Theory/factorizacion_prima.cpp
@@ -1,4 +1,6 @@
 //Factorizacion prima de un nuemero 
+//Para saber la cantidad de divisores de un numero n -> (ai1+1)*(ai2+1)*...(ak+1)
+//Para saber la suma de los dividores de un numero n -> (pi1)^(ai1+1)-1/pi-1*...(pi2)^(ai2+1)-1/pi2-1
 vector<int>factores;
 void factors(int &n){
 	for(int i=2; i*i<=n; i++){
